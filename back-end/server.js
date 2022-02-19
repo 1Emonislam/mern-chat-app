4044e512a3a8e5ed5e354f58f723133d6d7838cc
1emonislam <emonislamweb@gmail.com>
2022-02-19 16:14:43 +0600

    back end

---
+++ back-end/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+app.get('/',(req,res) =>{
+    res.send('server running')
+})
+app.listen(port, () => {
+    console.log('listening on',port);
+});
